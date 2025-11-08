9f8418263daa9074ccaee4dac1e578113b554859
Neve Callaway <nevecallaway@gmail.com>
2025-11-08 14:11:35 -0700

    Updated basics of index.js

--- service/index.js
+++ service/index.js
@@ -1,7 +1,18 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
 
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
 app.get('*', (_req, res) => {
   res.send({ msg: 'Portrait Portal service' });
