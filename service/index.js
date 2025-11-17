2be333e5eaf5152b7fea20c5da02738ee684601e
Neve Callaway <nevecallaway@gmail.com>
2025-11-16 18:41:54 -0700

    required database.js, deleted temporary data structures

--- service/index.js
+++ service/index.js
@@ -2,12 +2,10 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
+const DB = require('./database.js');
 const app = express();
 
 const authCookieName = 'token';
-
-let users = [];
-let commissions = [];
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
