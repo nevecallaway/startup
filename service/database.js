feacbba8b4c8aaf367c26719df9f10de0f887ce4
Neve Callaway <nevecallaway@gmail.com>
2025-11-16 18:33:38 -0700

    Created database.js and the db/collections/function to test connection

---
+++ service/database.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('portraitportal');
+const userCollection = db.collection('user');
+const commissionCollection = db.collection('commission');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
